6b7589b2bff7be2e68dc67124a0e2f103a62f071
Leevi Alanen <leevi.alanen1@gmail.com>
2024-02-10 19:29:05 +0200

    part1 complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -47,10 +47,23 @@ const Statistic = (props) => {
   }
 }  
 const App = () => {
+  // Anecdotes bank
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+  const [anecdoteVotes, setAnecdoteVotes] = useState(new Array(8).fill(0))
 
   const increaseGood = () => {
     console.log('value of good before', good)
@@ -67,8 +80,27 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const randomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const vote = () => {
+    const newVotes = [...anecdoteVotes]
+    newVotes[selected] += 1
+    setAnecdoteVotes(newVotes)
+    console.log('copy', newVotes)
+  }
+
   return (
     <div>
+      <h1>Anecdotes of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {anecdoteVotes[selected]} votes</p>
+      <Button handleClick={vote} text='vote' />
+      <Button handleClick={randomAnecdote} text='next anecdote' />
+      <h1>Anecdotes with most votes</h1>
+      <p>{anecdotes[anecdoteVotes.indexOf(Math.max(...anecdoteVotes))]}</p>
+      <p>has {Math.max(...anecdoteVotes)} votes</p>
       <h1>give feedback</h1>
       <Button handleClick={increaseGood} text='good' />
       <Button handleClick={increaseNeutral} text='neutral' />
